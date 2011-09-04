06daf243bc7c7d255676a29a915a7e3c9cbd54b3
Jesper Andre Lyngesen Pedersen <jalpedersen@gmail.com>
2011-09-04 21:03:43 +0400

    Upgrade to jetty 7.5.0

--- project.clj
+++ project.clj
@@ -1,7 +1,7 @@
-(defproject org.signaut/ring-jetty7-adapter "0.3.11.2"
+(defproject org.signaut/ring-jetty7-adapter "0.3.11.3"
   :description "Ring Jetty 7 adapter."
   :url "http://github.com/jalpedersen/ring-jetty7-adapter"
   :dependencies [[ring/ring-servlet "0.3.11"]
-                 [org.eclipse.jetty/jetty-server "7.4.4.v20110707"]]
+                 [org.eclipse.jetty/jetty-server "7.5.0.v20110901"]]
   :dev-dependencies [[clj-http "0.1.3"]
                      [lein-clojars "0.7.0"]])
