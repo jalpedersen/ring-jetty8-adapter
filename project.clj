f99265c26fe70e74179b638c2fc881e9641e991e
Jesper Andre Lyngesen Pedersen <jalpedersen@gmail.com>
2011-12-14 17:59:28 +0100

    1.0.0

--- project.clj
+++ project.clj
@@ -1,7 +1,7 @@
-(defproject org.signaut/ring-jetty7-adapter "1.0.0-RC1"
+(defproject org.signaut/ring-jetty7-adapter "1.0.0"
   :description "Ring Jetty 7 adapter."
   :url "http://github.com/jalpedersen/ring-jetty7-adapter"
-  :dependencies [[ring/ring-servlet "1.0.0-RC1"]
+  :dependencies [[ring/ring-servlet "1.0.0"]
                  [org.eclipse.jetty/jetty-server "7.5.4.v20111024"]]
   :dev-dependencies [[clj-http "0.1.3"]
                      [lein-clojars "0.7.0"]])
