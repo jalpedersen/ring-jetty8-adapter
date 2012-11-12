da155c37062eb1f8a1348d0c686631999a682159
Jesper Andre Lyngesen Pedersen <jalpedersen@gmail.com>
2012-11-13 00:11:16 +0100

    fix project.clj

--- project.clj
+++ project.clj
@@ -4,5 +4,5 @@
             :dependencies [[ring/ring-core "1.1.6" :exclusions [javax.servlet/servlet-api]]
                            [ring/ring-servlet "1.1.6" :exclusions [javax.servlet/servlet-api]]
                            [org.eclipse.jetty/jetty-server "8.1.8.v20121106"]]
-            :profiles {:dev {:dependencies [[clj-http "0.3.2"]
-                                            [lein-clojars "0.9.1"]]}})
+            :profiles {:dev {:dependencies [[clj-http "0.3.2"]]}}
+            :plugins [[lein-clojars "0.9.1"]])
